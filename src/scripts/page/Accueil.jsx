c6ebe5728e5a6ab9864c1e58ee93993a7d9a6d4e
Ylan <hei.nalisoa@gmail.com>
2022-09-16 17:32:53 +0300

    fix : fixing bug in dashboard css

--- src/scripts/page/Accueil.jsx
+++ src/scripts/page/Accueil.jsx
@@ -6,7 +6,7 @@ export default function HomePage () {
     return (
 <div>
     <div className="left"><SideBar/></div>
-    <div className="center">Accueil</div>
+    <div className="center2">Accueil</div>
 </div>
     )
 }
--- src/scripts/page/DashBoard.jsx
+++ src/scripts/page/DashBoard.jsx
@@ -1,14 +1,15 @@
 import SideBar from "../component/SideBar";
 import AllData from "../component/Tables";
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import '../../css/SideBar.css'
 
 export default function Dashboard () {
 
     return (
 <div>
     <div className="left"><SideBar/></div>
-    <div className="center"><AllData/>
-    </div>
-    
+    <div className="center"><AllData/></div>
 </div>
     )
 }
